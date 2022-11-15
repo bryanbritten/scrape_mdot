0632c21b119708107482c0e0b3ca02cb7664e402
Bryan <potent_pwnables@protonmail.com>
2022-11-14 22:18:54 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# scrape_mdot
-A simple script that pulls data from https://mdotjboss.state.mi.us. 
+# Overview
+
+This is a simple script that pulls down Michigan construction contract data from https://mdotjboss.state.mi.us/CCI. The intended purpose is to help automate the process of gathering this data for analysts that are interested in performing analytics on construction data. 
+
+# Implementation
+
+Because the MDOT site uses javascript to dynamically generate the data that is seen on the site, the use of Selenium is necessary in order to mimic the actions of a user. As of right now, Chrome is the only browser that is supported, but support for Firefox, Edge, IE, Opera, Brave, or any other major browser could be implemented.
+
+The script also attempts to do a lot of the heavy lifting for users, as the expectation is that users are not programmers by trade, and thus shouldn't be expected to do things like install Python libraries.
+
+# Usage
+
+In order to run the script, you must have Python installed on your computer. If you're using Windows, you can find a good tutorial on how to install Python [here](https://www.digitalocean.com/community/tutorials/install-python-windows-10). If you're on Mac, you can use [this article](https://docs.python-guide.org/starting/install3/osx/).
+
+Once you have Python installed, the script can be run with the following command.
+
+```bash
+python mdot_scraper.py
+```
+
+You'll be asked two questions:
+
+1. Where is the file with the project numbers located, and
+2. Where should the data be saved once it's downloaded.
+
+Both questions expect you to type file paths into the terminal. The filepath for the project numbers should point to a CSV or a TXT file, where each project number is on its own line. The filepath for the data output should be a folder where you want the CSV file(s) to be saved.
+
+# Handling Errors
+
+If you encounter any errors while using this script, it's almost certainly because the CSS/HTML for the site has changed. Feel free to [open an issue](https://github.com/potentpwnables/scrape_mdot/issues) with a screenshot of your Powershell window with as much output as you can show. 
---
+++ mdot_scraper.py
@@ -0,0 +1,157 @@
+import time
+import sys
+from pathlib import Path
+from winreg import HKEY_CURRENT_USER, OpenKey, QueryValueEx
+
+
+def install_library(lib_name) -> None:
+    import subprocess
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", lib_name])
+    print(
+        f"[+] {lib_name}, a required library, wasn't installed so it has been installed for you."
+    )
+
+
+try:
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import Select
+    from selenium.webdriver.support.wait import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+except ImportError:
+    install_library("selenium")
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import Select
+    from selenium.webdriver.support.wait import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+
+try:
+    import pandas as pd
+except ModuleNotFoundError:
+    install_library("pandas")
+    install_library("lxml")
+    import pandas as pd
+
+
+# stolen from https://stackoverflow.com/questions/19037216/how-to-get-a-name-of-default-browser-using-python
+def get_default_browser() -> str:
+    with OpenKey(
+        HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice",
+    ) as regkey:
+        browser = QueryValueEx(regkey, "ProgId")[0]
+
+    return browser
+
+
+def import_project_numbers() -> list[str]:
+    fpath = Path(input("Please provide the filepath to your list of project numbers: "))
+    while True:
+        if not fpath.exists():
+            print("[-] That filepath does not exist.")
+            fpath = Path(input("Please try again (press CTRL + C to exit): "))
+        elif fpath.is_dir():
+            print("[-] The filepath must point to a file, not a folder.")
+            fpath = Path(input("Please try again (press CTRL + C to exit): "))
+        elif fpath.suffix not in [".csv", ".txt"]:
+            print("[-] Project numbers must be stored in a CSV or TXT file.")
+            fpath = Path(input("Please try again (press CTRL + C to exit): "))
+        else:
+            break
+
+    with open(fpath, "r") as f:
+        project_numbers = f.readlines()
+
+    return map(str.strip, project_numbers)
+
+
+def set_output_directory():
+    fpath = Path(input("Please provide a folder where the data should be saved: "))
+    while True:
+        if not fpath.exists():
+            print("[-] That folder does not exist.")
+            fpath = Path(input("Please try again (press CTRL + C to exit): "))
+        elif not fpath.is_dir():
+            print("[-] That file path doesn't seem to point to a directory.")
+            fpath = Path(input("Please try again (press CTRL + C to exit): "))
+        else:
+            break
+
+    return str(fpath).rstrip("/")
+
+
+def get_number_of_pages(driver):
+    navigation = driver.find_element(By.CLASS_NAME, "page-navigation")
+    links = navigation.find_elements(By.TAG_NAME, "a")
+    return len(links) - 4
+
+
+def get_next_button(driver):
+    navigation = driver.find_element(By.CLASS_NAME, "page-navigation")
+    links = navigation.find_elements(By.TAG_NAME, "a")
+    return links[-2]
+
+
+def get_contract_data(driver, project_number):
+    BASE_URL = "https://mdotjboss.state.mi.us/CCI/"
+    driver.get(BASE_URL)
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "selectedReportType"))
+    )
+    report_type = Select(driver.find_element(By.ID, "selectedReportType"))
+    report_type.select_by_visible_text("Subcontracts")
+
+    contract_number = driver.find_element(By.ID, "contractProjectNum")
+    contract_number.clear()
+    contract_number.send_keys(project_number)
+    contract_number.send_keys(Keys.ENTER)
+
+    data = []
+    num_pages = get_number_of_pages(driver)
+    for i in range(num_pages):
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.ID, "subcontrtactsId"))
+        )
+
+        table = driver.find_element(By.ID, "subContractTable")
+        data.append(pd.read_html(table.get_attribute("outerHTML"))[0])
+        next_button = get_next_button(driver)
+        next_button.click()
+
+    df = pd.concat(data, ignore_index=True, sort=False)
+    return df
+
+
+if __name__ == "__main__":
+    project_numbers = import_project_numbers()
+    output_directory = set_output_directory()
+
+    # default_browser = get_default_browser()
+    # only support Chrome for the moment
+    default_browser = "ChromeHTML"
+    if default_browser == "ChromeHTML":
+        from selenium.webdriver.chrome.service import Service as ChromeService
+
+        try:
+            from webdriver_manager.chrome import ChromeDriverManager
+        except ImportError:
+            install_library("webdriver-manager")
+            from webdriver_manager.chrome import ChromeDriverManager
+
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    else:
+        print("This script can only run in Chrome. Please change your default browser.")
+        sys.exit(0)
+
+    for project_number in project_numbers:
+        data = get_contract_data(driver, project_number)
+        fpath = f"{output_directory}/{project_number}.csv"
+        data.to_csv(fpath, index=False)
+        time.sleep(3)
